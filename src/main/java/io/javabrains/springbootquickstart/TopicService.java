1ce9287ffc7719c10b39bedf3fa2dc9e83a452ba
benboaz1 <benedictboaz@gmail.com>
2025-01-27 09:48:43 -0600

    Initial commit for Spring Boot project

---
+++ src/main/java/io/javabrains/springbootquickstart/TopicService.java
@@ -0,0 +1,50 @@
+package io.javabrains.springbootquickstart;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    int x;
+    private List<Topic> topics = new ArrayList<>((Arrays.asList(
+            new Topic("spring", "spring framework", "spring framework description"),
+            new Topic("java", "Core Java", "Core Java description"),
+            new Topic("javaScript", "javaScript framework", "javaScript description")
+    )));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopicBen(int id) {
+        this.x = id;
+        return topics.get(x);
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+        ;
+    }
+}
